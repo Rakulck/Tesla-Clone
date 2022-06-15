ff9b8d11f2d34df9bd4efa33fd87ec9588ab29b0
Rakulck <77563090+Rakulck@users.noreply.github.com>
2022-06-15 23:47:25 +0530

    main

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,7 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+import image from "../model-s.jpg";
 
 function Section() {
-  return <div>Section</div>;
+  return (
+    <Wrap>
+      Section
+      {/* <img src={image} /> */}
+    </Wrap>
+  );
 }
 
 export default Section;
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url(${image});
+`;
